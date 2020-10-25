4be1035e9cedc64ec147a4f1c2e1e4f00c301666
Benjamin Higginbotham <benridesbikes@Benjamins-MacBook-Air.local>
2020-10-24 20:56:55 -0400

    add index route. add create route

--- controllers/items/items.js
+++ controllers/items/items.js
@@ -1,13 +1,19 @@
 //DEPENDENCIES
 const express = require('express');
+const { Model } = require('mongoose');
 const items = express.Router();
+const Items = require('../../models/items/items')
 
 //MONGOOSE
 
 //ROUTES
 //Index
-items.get('/', (req, res) => {
-    res.send(`Made it to the index page!`)
+items.get('/', async (req, res) => {
+    res.json(await Items.find({}))
+});
+//Create
+items.post('/', async (req, res) => {
+    res.json(await Items.create(req.body));
 });
 
 //EXPORT
